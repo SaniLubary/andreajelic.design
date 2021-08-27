487610b331cdc5711417820931649d720c85e622
santi lubary <santiago.lp.cop@gmail.com>
2021-08-27 17:59:50 -0300

    initial grid page structure+some example resources added + update readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import bikeGuy from './resources/bike-guy.jpg'
+import MainImageDescription from './components/MainImageDescription/MainImageDescription'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App grid-container">
+      {/* Left side */}
+      <div className="grid-item">  
+        <div className="vertical-sub-grid-containers">
+          {/* Logo */}
+          <div className="grid-item">logo</div>
+          {/* Menu, Imagen y Descripcion principal */}
+          <div className="grid-item">
+            <div className="center-sub-grid-container">
+              <div className="grid-item">a</div>
+              <div className="grid-item">
+                <div className="central-image-container">
+                  <img className="central-image" src={bikeGuy} alt="profesional guy riding bike on the dirt" />
+                </div>
+              </div>
+              <div className="grid-item">
+                <h3>LOEWE Esencia Eau de Parfum</h3>
+                <p>The full-set of the LOEWE Esencia family.</p>
+                <p>Get yours on perfumesloewe.com and select our exclusive complimentary wrapping.</p>
+              </div>
+            </div>
+          </div>
+          {/* Descripcion de la imagen principal, y numero de slider mostrado */}
+          <div className="grid-item">
+            <div className="center-sub-grid-container">
+              <div className="grid-item">a</div>
+              <div className="grid-item"><MainImageDescription></MainImageDescription></div>
+              <div className="grid-item">c</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Right side */}
+      <div className="grid-item right-side"></div>
     </div>
   );
 }
---
+++ src/components/MainImageDescription/MainImageDescription.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+
+function MainImageDescription() {
+  return (
+    <div className="main-image-description">
+      <div className="slider-number">
+        01/03
+      </div>
+      <div className="description">
+        Esencia Eau de Parfum and Eau de Toilette. The iconic masculine fragance of LOEWE.
+      </div>
+    </div>
+  );
+}
+
+export default MainImageDescription;
